ca94b9c1de9022b991b80b39dcf8b144a1dae379
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-18 16:41:23 -0500

    Openning connection

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'localhost:4000', 'https://concert-freak-ui.herokuapp.com/'
+    origins '*'
+    # origins 'localhost:4000', 'https://concert-freak-ui.herokuapp.com/'
 
     resource '*',
       headers: :any,
